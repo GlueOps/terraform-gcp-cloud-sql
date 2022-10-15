d869db1a2a497bd53d341a9fccdab54064792c1f
Venkata Mutyala <venkata.mutyala@glueops.dev>
2022-10-15 20:24:36 +0000

    pin rerun logic to a tag

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+module "rerun" {
+  source    = "git::https://github.com/GlueOps/terraform-toggle-rerun-for-tfc-operator.git?ref=v0.1.0"
+}
+
 
 provider "google" {
   project = local.project_name
--- rerun_logic.tf
+++
@@ -1,3 +0,0 @@
-module "rerun" {
-  source    = "git::https://github.com/GlueOps/terraform-toggle-rerun-for-tfc-operator.git"
-}
