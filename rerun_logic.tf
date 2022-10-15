8aa50fc6c8d70bfc9403b262631427805eb419a9
Venkata Mutyala <venkata.mutyala@glueops.dev>
2022-10-15 20:19:52 +0000

    fixes

--- rerun_logic.tf
+++ rerun_logic.tf
@@ -1,6 +1,3 @@
-variable "TOGGLE_TO_RERUN" { default = "" }
-
 module "rerun" {
   source    = "git::https://github.com/GlueOps/terraform-toggle-rerun-for-tfc-operator.git"
-  TOGGLE_TO_RERUN  = var.TOGGLE_TO_RERUN
 }
