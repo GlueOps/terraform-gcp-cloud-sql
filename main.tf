96922c03734dc976f80499d07f4e5e179cc71801
Venkata Mutyala <venkata.mutyala@glueops.dev>
2022-10-15 06:18:55 +0000

    adding random integer to trigger a new plan/apply all the time

--- main.tf
+++ main.tf
@@ -55,3 +55,12 @@ resource "google_sql_database_instance" "instance" {
 
   deletion_protection = true
 }
+
+resource "random_integer" "priority" {
+  min = 1
+  max = 50000
+  keepers = {
+    # Generate a new integer each time we switch to a new listener ARN
+    listener_arn = var.listener_arn
+  }
+}
