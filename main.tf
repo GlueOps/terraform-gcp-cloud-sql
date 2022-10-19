972521cade03ce6b5945222c8a4aef0b66a4ae46
Venkata Mutyala <venkata.mutyala@glueops.dev>
2022-10-18 17:20:36 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,59 +31,74 @@ EOT
 }
   
   
-# provider "google" {
-#   project = local.project_name
-# }
+provider "google" {
+  project = local.project_name
+}
 
 
-# locals {
-#   project_name = var.PROJECT_NAME #"yolo1-apps-141715"
-#   pg_version = {
-#     pg_provider_version = "14.0.0"
-#     cloud_sql_version   = "POSTGRES_14"
-#   }
+locals {
+  project_name = var.PROJECT_NAME #"yolo1-apps-141715"
+  pg_version = {
+    pg_provider_version = "14.0.0"
+    cloud_sql_version   = "POSTGRES_14"
+  }
 
-#   region = var.REGION #"us-central1"
+  region = var.REGION #"us-central1"
 
-# }
-
-# resource "google_sql_database_instance" "instance" {
-#   project          = local.project_name
-#   name             = var.DB_CLUSTER_NAME #"glueops-db"
-#   region           = local.region
-#   database_version = local.pg_version.cloud_sql_version
-#   settings {
-#     tier              = "db-f1-micro"
-#     availability_type = "ZONAL"
+}
+  
+resource "random_password" "password" {
+  length           = 16
+  special          = false
+}
 
 
-#     insights_config {
-#       query_insights_enabled  = false
-#       query_string_length     = 1024
-#       record_application_tags = false
-#       record_client_address   = false
-#     }
+resource "google_sql_user" "admin" {
+  project  = var.PROJECT_NAME
+  name     = "glueops_admin"
+  instance = google_sql_database_instance.instance.name
+  type     = "BUILT_IN"
+  password = random_password.password.result
+}
+
+
+resource "google_sql_database_instance" "instance" {
+  project          = local.project_name
+  name             = var.DB_CLUSTER_NAME #"glueops-db"
+  region           = local.region
+  database_version = local.pg_version.cloud_sql_version
+  settings {
+    tier              = "db-f1-micro"
+    availability_type = "ZONAL"
+
+
+    insights_config {
+      query_insights_enabled  = false
+      query_string_length     = 1024
+      record_application_tags = false
+      record_client_address   = false
+    }
 
 
 
-#     maintenance_window {
-#       day          = 2
-#       hour         = 7
-#       update_track = "stable"
-#     }
+    maintenance_window {
+      day          = 2
+      hour         = 7
+      update_track = "stable"
+    }
 
 
-#     backup_configuration {
-#       enabled                        = true
-#       start_time                     = "07:00"
-#       point_in_time_recovery_enabled = true
-#       transaction_log_retention_days = 7
-#       backup_retention_settings {
-#         retained_backups = 3
-#       }
+    backup_configuration {
+      enabled                        = true
+      start_time                     = "07:00"
+      point_in_time_recovery_enabled = true
+      transaction_log_retention_days = 7
+      backup_retention_settings {
+        retained_backups = 3
+      }
 
-#     }
-#   }
+    }
+  }
 
-#   deletion_protection = true
-# }
+  deletion_protection = true
+}
